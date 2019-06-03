28b07b1c0e6b0de0c3d76e77b018adfee19b518c
frransier <frransier@gmail.com>
2019-06-03 23:41:25 +0200

    refactored to useReducer

--- web/src/pages/game.js
+++ web/src/pages/game.js
@@ -1,6 +1,6 @@
-import React, { useState, useEffect, useCallback } from "react";
+import React, { useState, useEffect, useReducer } from "react";
 import { graphql } from "gatsby";
-import { mapEdgesToNodes } from "../lib/helpers";
+import { mapEdgesToNodes, dynamicSort } from "../lib/helpers";
 import BlogPostPreviewGrid from "../components/blog-post-preview-grid";
 import Container from "../components/container";
 import GraphQLErrorList from "../components/graphql-error-list";
@@ -11,12 +11,13 @@ import { responsiveTitle1, responsiveTitle2 } from "../components/typography.mod
 
 export const query = graphql`
   query GamePageQuery {
-    players: allSanityPlayer(sort: { fields: team }, filter: { included: { eq: true } }) {
+    team: allSanityPlayer(sort: { fields: team }, filter: { included: { eq: true } }) {
       edges {
         node {
           name
           team
           ppg
+          id
         }
       }
     }
@@ -33,31 +34,63 @@ const GamePage = props => {
     );
   }
 
-  const [playerNodes, setPlayerNodes] = useState(
-    data && data.players && mapEdgesToNodes(data.players)
-  );
+  const playerNodes = data && data.team && mapEdgesToNodes(data.team);
 
-  const [players, setPlayers] = useState([]);
   const [fullTeam, setFullTeam] = useState(false);
 
+  const [players, playerDispatch] = useReducer((state, action) => {
+    switch (action.type) {
+      case "add":
+        if (state.includes(action.player)) {
+          return state;
+        } else {
+          return [...state, action.player].sort(dynamicSort("name"));
+        }
+      case "remove":
+        return state.filter(player => player.id !== action.id);
+    }
+  }, playerNodes.sort(dynamicSort("name")));
+
+  const [team, teamDispatch] = useReducer((state, action) => {
+    switch (action.type) {
+      case "add":
+        if (state.includes(action.player) || state.length > 4) {
+          return state;
+        } else {
+          playerDispatch({
+            type: "remove",
+            id: action.player.id
+          });
+          return [...state, action.player];
+        }
+
+      case "remove":
+        playerDispatch({
+          type: "add",
+          player: state[action.index]
+        });
+        return state.filter((_, index) => index != action.index);
+      default:
+        return state;
+    }
+  }, []);
+
   useEffect(() => {
-    players.length === 5 ? setFullTeam(true) : setFullTeam(false);
-  }, [players]);
+    team.length === 5 ? setFullTeam(true) : setFullTeam(false);
+  }, [team]);
 
   function addPlayer(player) {
-    fullTeam ? null : setPlayers(prevPlayers => [...prevPlayers, player]);
-    filterPlayer(player);
+    teamDispatch({
+      type: "add",
+      player
+    });
   }
 
   function removePlayer(index) {
-    setPlayerNodes(prevNodes => [...prevNodes, players[index]]);
-    players.splice(index, 1);
-    setPlayers([...players]);
-  }
-
-  function filterPlayer(player) {
-    const updatedPlayerNodes = playerNodes.filter(p => p.name !== player.name);
-    setPlayerNodes([...updatedPlayerNodes]);
+    teamDispatch({
+      type: "remove",
+      index
+    });
   }
 
   return (
@@ -67,8 +100,8 @@ const GamePage = props => {
         <div>
           <h3 className={responsiveTitle2}>Your team </h3>
           <hr />
-          {players.map((player, index) => (
-            <div key={index} value={player.name} onClick={() => removePlayer(index)}>
+          {team.map((player, index) => (
+            <div key={player.id} value={player.name} onClick={() => removePlayer(index)}>
               {player.name}
             </div>
           ))}
@@ -81,35 +114,35 @@ const GamePage = props => {
           >
             <input type="hidden" name="form-name" value="play" />
             <input
-              value={players.length > 4 && players[0] && players[0].name}
+              value={team.length > 4 && team[0].name}
               readOnly={true}
               type="hidden"
               name="Player 1"
               required
             />
             <input
-              value={players.length > 4 && players[1] && players[1].name}
+              value={team.length > 4 && team[1].name}
               readOnly={true}
               type="hidden"
               name="Player 2"
               required
             />
             <input
-              value={players.length > 4 && players[2] && players[2].name}
+              value={team.length > 4 && team[2].name}
               readOnly={true}
               type="hidden"
               name="Player 3"
               required
             />
             <input
-              value={players.length > 4 && players[3] && players[4].name}
+              value={team.length > 4 && team[3].name}
               readOnly={true}
               type="hidden"
               name="Player 4"
               required
             />
             <input
-              value={players.length > 4 && players[4] && players[4].name}
+              value={team.length > 4 && team[4].name}
               readOnly={true}
               type="hidden"
               name="Player 5"
@@ -130,8 +163,8 @@ const GamePage = props => {
         </div>
         <hr />
         <div>
-          <h2 className={responsiveTitle1}>Pick your players</h2>
-          {playerNodes.map((player, index) => (
+          <h2 className={responsiveTitle1}>Pick your team</h2>
+          {players.map((player, index) => (
             <div key={index}>
               <p onClick={() => addPlayer(player)}>{player.name}</p>
             </div>
